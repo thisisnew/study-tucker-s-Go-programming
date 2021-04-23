e6904a30f15167ab69dc488b7296355b3dcf77ae
thisisnew <thisisnew@naver.com>
2021-04-23 16:17:57 +0900

    자료구조

---
+++ ch22/ex22.2/ex22.2.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type Queue struct {
+	v *list.List
+}
+
+func (q *Queue) push(val interface{}) {
+	q.v.PushBack(val)
+}
+
+func (q *Queue) Pop() interface{} {
+	front := q.v.Front()
+	if front != nil {
+		return q.v.Remove(front)
+	}
+
+	return nil
+}
+
+func NewQueue() *Queue {
+	return &Queue{list.New()}
+}
+
+func main() {
+	queue := NewQueue()
+
+	for i := 1; i < 5; i++ {
+		queue.push(i)
+	}
+
+	v := queue.Pop()
+	for v != nil {
+		fmt.Printf("%v -> ", v)
+		v = queue.Pop()
+	}
+}
